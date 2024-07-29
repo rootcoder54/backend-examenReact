7966211dfd8f1f13b18b0af1f56f83d5a70f8f9a
ozymandias54 <bassirou718@gmail.com>
2024-07-29 00:14:02 +0000

    cloudinary

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,21 +3,9 @@ import userModel from '../models/userModel.js';
 
 const addUser = async(req,res) => {
     try {
-        const nom = req.body.nom;
-        const username = req.body.username;
-        const password = req.body.password;
-
-        const userData = {
-            nom,
-            username,
-            password
-        }
-
-        const user = userModel(userData);
+        const user = userModel(req.body);
         await user.save();
-
         res.json({success:true, message:"user is Added"})
-
 
     } catch (error) {
         res.json({success:false});
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import upload from "../middleware/multer.js";
 import { addUser, listUser, removeUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
